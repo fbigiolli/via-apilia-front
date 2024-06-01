7100d9857662dc3a3755c125f2d92e2756411a3f
fbigiolli <felipebigiolli@gmail.com>
2024-05-31 21:32:22 -0300

    create SeleccionDePote responsive page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,57 @@
+import React, { useState } from 'react';
 import PoteCard from './components/PoteCard/PoteCard';
-import logo from './logo.svg';
-import { Card , Button } from 'react-bootstrap';
+import './App.css';
 
 function App() {
+  const [selectedPote, setSelectedPote] = useState(null);
+
+  const handleCardClick = (cardId) => {
+    setSelectedPote(cardId);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <PoteCard 
-          imageSrc="/helados-1-4.jpg" 
-          title="1/4 Kilo" 
-          subtitle="Máximo 3 gustos" 
-        />
-      </header>
+      <div className="container px-5">
+        <div className='row justify-content-center d-flex'>
+          <div className='col-md-12 mt-5 text-center'>
+            <div className='header-title'>Paso 1/2</div>
+            <div className='header-subtitle'>Seleccioná el tamaño del pote</div>
+          </div>
+        </div>
+
+        <div className="row mt-5 justify-content-center mx-auto gap-0 ">
+          <div className="col-xs-12 col-sm-6 col-lg-3 mb-3">
+            <PoteCard
+              imageSrc="/helados-1-4.jpg"
+              title="1/4 Kilo"
+              subtitle="Máximo 3 gustos"
+              cardId="250"
+              isSelected={selectedPote === "250"}
+              onClick={handleCardClick}
+            />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-lg-3 mb-3">
+            <PoteCard
+              imageSrc="/helados-1-4.jpg"
+              title="1/2 Kilo"
+              subtitle="Máximo 3 gustos"
+              cardId="500"
+              isSelected={selectedPote === "500"}
+              onClick={handleCardClick}
+            />
+          </div>
+          <div className="col-xs-12 col-sm-6 col-lg-3 mb-3">
+            <PoteCard
+              imageSrc="/helados-1-4.jpg"
+              title="1 Kilo"
+              subtitle="Máximo 4 gustos"
+              cardId="1000"
+              isSelected={selectedPote === "1000"}
+              onClick={handleCardClick}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/PoteCard/PoteCard.jsx
+++ src/components/PoteCard/PoteCard.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './PoteCard.css';
 
-const PoteCard = ({ imageSrc, title, subtitle }) => {
-    return (
-        <Card className="custom-card">
-            <Card.Img
-                variant="top"
-                src={imageSrc}
-                className="custom-card-img"
-            />
-            <Card.Body className="custom-card-body">
-                <Card.Title className="custom-card-title">
-                    {title}
-                </Card.Title>
-                <Card.Text className="custom-card-subtitle">
-                    {subtitle}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    );
+const PoteCard = ({ imageSrc, title, subtitle, cardId, isSelected, onClick }) => {
+
+  return (
+    <Card className={`pote-card ${isSelected ? 'selected' : ''}` } col-sm-12 col-md-4 mb-4 onClick={() => onClick(cardId)}>
+      <Card.Img variant="top" src={imageSrc} className="pote-card-img" />
+      <Card.Body>
+        <Card.Title className="pote-card-title">{title}</Card.Title>
+        <Card.Text className="pote-card-subtitle">{subtitle}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default PoteCard;
